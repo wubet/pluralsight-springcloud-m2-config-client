8cbda88e7077c82a4e362398d1f92e6f74c85afd
wubet <wube.tuffa@gmail.com>
2019-10-11 14:26:45 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# pluralsight-springcloud-m2-config-client
+ it is a spring cloud config client microservice
---
+++ src/main/java/pluralsight/demo/PluralsightSpringcloudM2ConfigClientApplication.java
@@ -0,0 +1,14 @@
+package pluralsight.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PluralsightSpringcloudM2ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PluralsightSpringcloudM2ConfigClientApplication.class, args);
+	}
+
+}
+
